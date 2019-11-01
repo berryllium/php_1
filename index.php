5ada3b4763d5b80cfd23b3eebbdf6357967fae35
berryllium <gorkundp@yandex.ru>
2019-11-01 17:09:54 +0300

    ссылки на уроки

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <?php
-$a = 4;
-$b = 5;
-echo '<br>';
-
-echo '<br>';
-var_dump($a === $b); // Сравнение по значению и типу
-echo '<br>';
-var_dump($a == $b);  // Сравнение по значению
-echo '<br>';
-var_dump($a > $b);    // Больше
-echo '<br>';
-var_dump($a < $b);    // Меньше
-echo '<br>';
-var_dump($a <> $b);  // Не равно
-echo '<br>';
-var_dump($a != $b);   // Не равно
-echo '<br>';
-var_dump($a !== $b); // Не равно без приведения типов
-echo '<br>';
-var_dump($a <= $b);  // Меньше или равно
-echo '<br>';
-var_dump($a >= $b);  // Больше или равно
+header('Content-Type: text/html; charset=utf-8');
+echo 'Задание к первому уроку <a href="hw1">по ссылке</a>';
 ?>
